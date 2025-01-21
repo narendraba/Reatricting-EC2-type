2c3b355701d40d2348951648b1e566e3af0a531a
narendraba <ballipallinarendra@gmail.com>
2025-01-21 14:29:26 +0530

    Update varaibles.tf

--- compute/varaibles.tf
+++ compute/varaibles.tf
@@ -8,12 +8,3 @@ variable  "instance_type" {
   type          = string
 }
 
-variable  "instance_count" {
-  description   = "Number of ec2 instance to be launched"
-  type          = string
-}
-
-variable  "instance_name" {
-  description   = "Name of the ec2 instance"
-  type          = string
-}
